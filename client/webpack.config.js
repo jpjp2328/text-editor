a0b8c3c3177139e9ab8316132b46bb86d4a79f31
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-07 20:46:09 +1100

    added webpack manifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -22,16 +22,35 @@ module.exports = () => {
         template: './index.html',
         title: 'Text Editor'
       }),
-
+      // Add and configure workbox plugins for a service worker and manifest file.
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Edit your text',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
     ],
 
     module: {
       rules: [
-        
+
       ],
     },
   };
