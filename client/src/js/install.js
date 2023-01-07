dd51447deb4fecb66cfb66c7f38cbc5312776178
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-07 23:24:36 +1100

    butInstall event handler updated

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -10,7 +10,22 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    // Show prompt
+    promptEvent.prompt();
+
+    // Reset the deferred prompt variable
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => { });
